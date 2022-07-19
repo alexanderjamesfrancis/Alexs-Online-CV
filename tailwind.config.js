9de10b42fe2efb5312b0440a995d5ad8a602c8af
Alex Francis <alexanderjamesfrancis@hotmail.co.uk>
2022-07-19 13:24:09 +0100

    Add Js script

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    fontFamily: {
+      serif: ['Merriweather','sans-serif']
+    },
+    extend: {
+      
+    },
   },
   plugins: [],
 }
